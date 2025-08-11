8b904cf9232adeb0c31781f20a091870780496a5
ricardo.urbano <ricardo.urbano@cejam.org.br>
2025-08-11 18:09:13 -0300

    aula configuração iniciais api - testando login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\UnauthorizedException;
 
 class LoginController extends Controller
 {
     public function login(Request $request ) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
 
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw UnauthorizedException::withMessages([
+                'email' => 'The provided credentials are incorrect.',
+            ]);
+        }
+        $token = $user->createToken('apitoken')->plainTextToken;
+
+        return ['token' => $token];
     }
 }
